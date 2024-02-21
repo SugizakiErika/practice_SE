47d74a1fe5adc4f99e4d262ece58947e3fe00cfc
SugizakiErika <arsontable119@gmail.com>
2024-02-21 19:10:42 +0900

    カリキュラム09-1とcsrfセッションエラー対応処理

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -10,20 +10,28 @@
     <body>
         <h1 class="title">編集画面</h1>
         <div class="content">
+            <!--csrfトークンのセッション切れ用-->
+            @if (session('error'))
+                <div class="alert alert-danger" style="color:red">{{ session('error') }}</div>
+                <pre><code>{{ var_dump(session()->get('_old_input')) }}</code></pre>
+                <!--var_dump:指定した変数や配列に関してその型や値を含む構造化された情報を出力する-->    
+            @endif
             <form action="/posts/{{ $post->id }}" method="POST">
-                @csrf
+                
                 @method('PUT')
                 <div class='content__title'>
                     <h2>タイトル</h2>
-                    <input type='text' name='post[title]' value="{{ $post->title }}">
+                    <input type='text' name='post[title]' value="{{ old(('post.title'),$post->title) }}">
+                
                 </div>
                 <div class='content__body'>
                     <h2>本文</h2>
-                    <input type='text' name='post[body]' value="{{ $post->body }}">
+                    <input type='text' name='post[body]' value="{{ old(('post.body'),$post->body) }}">
                 </div>
                 <input type="submit" value="保存">
             </form>
         </div>
+
     </body>
 </html>
     
